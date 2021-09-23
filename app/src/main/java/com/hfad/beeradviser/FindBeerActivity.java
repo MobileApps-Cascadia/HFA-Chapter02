0c4bed18b20a700f5e26cae9795215ae72163ea2
Brian Bansenauer <brianb@cascadia.edu>
2021-09-23 14:57:27 -0700

    updates gradle and api

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -3,6 +3,7 @@ package com.hfad.beeradviser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+
     }
 
     //Call when the button gets clicked
