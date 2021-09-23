32bec3eceec7e6e1891cc0e290c5892a12b57a08
Brian Bansenauer <brianb@cascadia.edu>
2021-09-23 15:42:44 -0700

    updates BeerExpert to return string

--- app/src/main/java/com/hfad/beeradviser/BeerExpert.java
+++ app/src/main/java/com/hfad/beeradviser/BeerExpert.java
@@ -9,15 +9,32 @@ import java.util.List;
 
 public class BeerExpert {
 
-    List<String> getBrands(String color) {
-        List<String> brands = new ArrayList<>();
-        if (color.equals("amber")) {
-            brands.add("Jack Amber");
-            brands.add("Red Moose");
-        } else {
-            brands.add("Jail Pale Ale");
-            brands.add("Gout Stout");
+    List<String> brandList = new ArrayList<>();
+
+    String getBrands(String color) {
+        //Build the brand recommendation List
+        switch (color) {
+            case "light":
+                brandList.add("Jail Pale Ale");
+                brandList.add("Gout Stout");
+                break;
+            case "amber":
+                brandList.add("Jack Amber");
+                brandList.add("Red Moose");
+                break;
+            case "brown":
+                brandList.add("Undercover Ale");
+                break;
+            case "dark":
+                brandList.add("Peanut butter Stout");
+                brandList.add("Guinness");
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + color);
         }
+        //Create a simple string from the List
+        String brands = "";
+        for(String brand : brandList) brands += brand + " \n";
         return brands;
     }
 }
